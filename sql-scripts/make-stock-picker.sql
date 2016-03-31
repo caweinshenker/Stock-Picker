024b8f33e16f5350be6f6589f04fa4bb9b2cad7c
Colin Weinshenker <caweinshenker@email.wm.edu>
2016-03-31 14:35:48 -0400

    Add db build script

---
+++ sql-scripts/make-stock-picker.sql
@@ -0,0 +1,79 @@
+delete from stock;
+drop table stock;
+
+delete from stock_price;
+drop table stock_price;
+
+delete from stock_dividend;
+drop table stock_dividend;
+
+delete from text;
+drop table text;
+
+delete from book;
+drop table book;
+
+delete from poem;
+drop table poem;
+
+delete from lyric;
+drop table lyric;
+
+delete from custom;
+drop table custom;
+
+CREATE TABLE stock(
+  ticker         varchar(10)  NOT NULL,
+  stock_index    varchar(20)  NOT NULL DEFAULT "no index",
+  company_name   varchar(100) NOT NULL DEFAULT "no name",
+  PRIMARY KEY    (ticker, stock_index)
+);
+
+CREATE TABLE stock_price(
+  ticker         varchar(10) NOT NULL,
+  pdate          date        NOT NULL,
+  open_price     numeric(10),
+  close_price    numeric(10),
+  PRIMARY KEY    (ticker, pdate),
+  FOREIGN KEY (ticker) REFERENCES stock(ticker)
+);
+
+CREATE TABLE stock_dividend(
+  ticker        varchar(10) NOT NULL,
+  ddate         date        NOT NULL,
+  ex_date       date        NOT NULL,
+  price         numeric(10) NOT NULL,
+  PRIMARY KEY   (ticker, ddate),
+  FOREIGN KEY   (ticker) REFERENCES stock(ticker)
+);
+
+CREATE TABLE text(
+  author_name    varchar(100),
+  file_location  varchar(100) NOT NULL PRIMARY KEY,
+  description    text,
+  title          varchar(100),
+  word_count     int,
+);
+   
+
+CREATE TABLE book(
+  pub_date       date,
+  ISBN           numeric(13)
+)INHERITS(text);
+
+CREATE TABLE poem(
+  pub_date       date
+)INHERITS(text);
+
+CREATE TABLE lyric(
+  song           varchar(50),
+  album          varchar(50),
+ )INHERITS(text);
+
+CREATE TABLE custom(
+ )INHERITS(text);
+
+  
+  
+
+
