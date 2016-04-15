94661a4992df36975cf549a90a9d6ba11029a94c
maxmiroff <maxmiroff@gmail.com>
2016-04-14 23:34:06 -0400

    Fixed trie parser for special characters in .txt files

--- stock_picker/helpers/parse_text_for_portfolio.py
+++ stock_picker/helpers/parse_text_for_portfolio.py
@@ -3,48 +3,50 @@ import psycopg2
 import getpass
 import sys
 import csv
+import re
+
 
 def parse_text(text, trie):
-''' given a text location and a trie of tickers, returns a dictionary where the keys are tickers
-    and the values are the occurences of the ticker within the words of the text '''
 	tickerDict = {}
 	with open(text, "r") as text:
 		for line in text:
+			line = re.sub('[^A-Za-z0-9]+', '', line)		
 			for word in line.split(" "):
 				word = word.upper()
 				wordLen = len(word)
-				index = 0
-				subString = "" + word[index]
-				endOfWord = False
-				successfulParse = True
-				while(len(subString) > 0 & endOfWord == False):
-					while(subString in trie):
-						successfulParse = True
-						if (subString in tickerDict):
-							tickerDict[subString] += 1
-						else:
-							tickerDict[subString] = 1
-						if(index < (wordLen - 1)):
-							index += 1
-							subString = subString + word[index]
-						elif (index == (wordLen- 1)):
-							subString = ""
-							endOfWord = True
-					if(endOfWord == True):
-						subString = ""
-					else:
-						if(successfulParse == False):
+				if(len(word) > 0):
+					index = 0
+					subString = "" + word[index]
+					endOfWord = False
+					successfulParse = True
+					while(len(subString) > 0 & endOfWord == False):
+						while(subString in trie):
+							successfulParse = True
+							if (subString in tickerDict):
+								tickerDict[subString] += 1
+							else:
+								tickerDict[subString] = 1
 							if(index < (wordLen - 1)):
 								index += 1
-							else:
+								subString = subString + word[index]
+							elif (index == (wordLen- 1)):
+								subString = ""
 								endOfWord = True
-						subString = "" + word[index]
-					successfulParse = False
+						if(endOfWord == True):
+							subString = ""
+						else:
+							if(successfulParse == False):
+								if(index < (wordLen - 1)):
+									index += 1
+								else:
+									endOfWord = True
+							subString = "" + word[index]
+						successfulParse = False
 	return tickerDict
 
-'''	
+'''
 def main():
-	text = "../texts/test.txt"
+	text = "../uploads/flatland.txt"
 	tickerTrie = trieMaker.query_database_for_tickers()
 	print(tickerTrie)
 	portfolioList = parse_text(text, tickerTrie)
